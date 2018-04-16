f5e5a2cb531886f0bb9e89a2b06d28fae3eeebae
Max Franz <maxkfranz@gmail.com>
2018-04-16 12:32:24 -0400

    Manually remove node_modules

--- node_modules/webcola/dist/src/handledisconnected.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function applyPacking(graphs: Array<any>, w: any, h: any, node_size: any, desired_ratio?: number): void;
-export declare function separateGraphs(nodes: any, links: any): any[];
