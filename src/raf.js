32ef15967847e5d2f8974701f40a819b4b8f4b9b
Max Franz <maxkfranz@gmail.com>
2018-10-24 16:41:56 -0400

    Better rad polyfill for old browsers

--- src/raf.js
+++ src/raf.js
@@ -4,7 +4,8 @@ if( typeof window !== typeof undefined ){
   raf = ( window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame ||
-    window.msRequestAnimationFrame
+    window.msRequestAnimationFrame ||
+    (fn => setTimeout(fn, 16))
   );
 } else { // if not available, all you get is immediate calls
   raf = function( cb ){
